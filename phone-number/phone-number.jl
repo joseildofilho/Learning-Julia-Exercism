1993dd68f77ca994df0729155ce05a58935e9c16
joseildo filho <joseildofilho@yahoo.com.br>
2019-08-01 11:03:37 -0300

    some little improvement

--- phone-number/phone-number.jl
+++ phone-number/phone-number.jl
@@ -1,12 +1,7 @@
 function get_number(number)
-	value = ""
-	for m in eachmatch(r"\d+", number)
-		value *= m.match
-	end
-	if value[1] == '1'
-		value = value[2:end]
-	end
-	return value
+	number = filter(d -> isdigit(d), number)
+	number = first(number) == '1' ? number[2:end] : number
+	return number
 end
 
 valid_number = r"\s*\+?1?\s*\(?\s*[2-9]\d{2}\s*\)?\s*\.?[2-9]\d{2}\s*-?\.?\s*\d{4}\s*"
@@ -18,5 +13,3 @@ function clean(phone_number)
 		return phone_number
 	end
 end
-
-
